ebbd35091a48bfd9189f753a6a10e8db2275ccb5
eepy <eepy2.23@gmail.com>
2025-01-17 03:24:31 +0900

    ✨ Refactor: Un-comment and integrate previously commented components in App.tsx, enabling full functionality of Counter, ToggleSection, WeatherSection, FeedbackForm, and RegistrationForm with toggle feature.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-// import Counter from "./components/Counter";
-// import { ToggleSection } from "./components/ToggleSection";
-// import { WeatherSection } from "./components/WeatherSection";
+import Counter from "./components/Counter";
+import { ToggleSection } from "./components/ToggleSection";
+import { WeatherSection } from "./components/WeatherSection";
 
-// import { FeedbackForm } from "./components/FeedbackForm";
-// import { RegistrationForm } from "./components/RegistrationForm";
-// import useToggle from "./hooks/useToggle";
+import { FeedbackForm } from "./components/FeedbackForm";
+import { RegistrationForm } from "./components/RegistrationForm";
+import useToggle from "./hooks/useToggle";
 
 import { ThemeToggleButton } from "./components/ThemeToggleButton";
 import { ThemeProvider } from "./context/useTheme";
 
 function App() {
-  // const [isToggled, toggle] = useToggle();
+  const [isToggled, toggle] = useToggle();
 
-  // function renderForm() {
-  //   if (isToggled) return <FeedbackForm />;
-  //   return <RegistrationForm />;
-  // }
+  function renderForm() {
+    if (isToggled) return <FeedbackForm />;
+    return <RegistrationForm />;
+  }
 
   return (
     <ThemeProvider>
       <div>
-        {/* <Counter /> */}
-        {/* <ToggleSection /> */}
-        {/* <WeatherSection /> */}
-        {/* <div>
-        <button
-          onClick={() => {
-            toggle();
-          }}>
-          Show {isToggled ? "Registration" : "Feedback"}
-        </button>
-        {renderForm()}
-      </div> */}
+        <Counter />
+        <ToggleSection />
+        <WeatherSection />
+        <div>
+          <button
+            onClick={() => {
+              toggle();
+            }}>
+            Show {isToggled ? "Registration" : "Feedback"}
+          </button>
+          {renderForm()}
+        </div>
         <ThemeToggleButton />
       </div>
     </ThemeProvider>
